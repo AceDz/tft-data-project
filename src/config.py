d55461888b609868a43127aa7493e09ab46a5801
AceDz <acepucela@gmail.com>
2026-05-27 17:24:05 +0200

    configurated .env, .gitignore, deleted pipeline

--- src/config.py
+++ src/config.py
@@ -1,9 +1,7 @@
 import os
 
-API_KEY = os.getenv("RIOT_API_KEY", "RGAPI-bb9fa92d-26aa-4df1-9fa6-b43e986387c8")
-
 HEADERS = {
-    "X-Riot-Token": API_KEY
+    "X-Riot-Token": os.getenv("RIOT_API_KEY")
 }
 
 REGION = "europe"
